b245a2140eb1ba5aeee37f06dae3bff224ef7311
minalonthego <82099586+minalonthego@users.noreply.github.com>
2025-04-11 09:08:12 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+# Install Tesseract
+apt-get update && apt-get install -y tesseract-ocr
+
+# Install Python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
